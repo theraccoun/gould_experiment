fa3f9f1c37de2969365f8267cf1c5797c6e87e94
Steven MacCoun <theraccoun@gmail.com>
2013-02-27 02:42:09 -0700

    Full implementation of instruction images

--- src/InstructionPanel.java
+++ src/InstructionPanel.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -17,45 +18,54 @@ public class InstructionPanel extends JPanel{
     private JLabel instructions;
     private JButton nextButton;
     private JButton prevButton;
+    private static final Font buttonFont = new Font("Serif", Font.BOLD, 18);
 
 
     public InstructionPanel(String imgPath) {
 
-        this.setLayout(null);
+        this.setBackground(new Color(173, 216, 230));
+
+        imgPath = "resources" + File.separator + imgPath;
 
         try {
-            InputStream is = ClassLoader.getSystemResourceAsStream(imgPath);
-            BufferedImage bi  = ImageIO.read(is);
+            File f = new File(imgPath);
+            if(!f.exists())
+                System.out.println(imgPath + " does not exist!");
+            BufferedImage bi  = ImageIO.read(f);
             this.instructions = new JLabel(new ImageIcon(bi));
-            this.instructions.setPreferredSize(new Dimension(this.getWidth(), this.getHeight()));
-            this.add(instructions);
         } catch (IOException ex) {
             System.out.println("Unable to load instruction image " + imgPath);
             ex.printStackTrace();
         }
 
-
-
         this.nextButton = new JButton("Next");
+        this.nextButton.setOpaque(true);
         this.nextButton.setVisible(true);
+        this.nextButton.setFont(buttonFont);
+        this.nextButton.setPreferredSize(new Dimension(150, 100));
         this.nextButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 Simulator.getInstance().executeNextLine();
             }
         });
-        this.add(nextButton);
 
 
-        this.prevButton = new JButton("Prev");
+        this.prevButton = new JButton("Previous");
+        this.prevButton.setOpaque(true);
         this.prevButton.setVisible(true);
+        this.prevButton.setFont(buttonFont);
+        this.prevButton.setPreferredSize(new Dimension(150, 100));
         this.prevButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 Simulator.getInstance().executePrevLine();
             }
         });
+
         this.add(prevButton);
+        this.add(instructions);
+        this.add(nextButton);
 
         requestFocusInWindow();
         setKeyBindings();
@@ -75,25 +85,5 @@ public class InstructionPanel extends JPanel{
             }
         });
     }
-
-//    public void paintComponent(Graphics g)
-//    {
-//        super.paintComponent(g);
-//        setButtonPositions();
-//    }
-
-
-//    private void setButtonPositions(){
-//
-//        int buttonWidth = (int)(padD.getWidth()*0.2);
-//        int buttonHeight = (int)(padD.getHeight()*0.1);
-//        int buttonY = (int)(panelP2Y + borderThickness/2) - 10;
-//
-//        int nButton1X = (int)(panelP2X) - 10;
-//        nextButton.setBounds(nButton1X, buttonY, buttonWidth, buttonHeight);
-//
-//        int pButton1X = (int)(panelP1X + borderThickness/2) + 10;
-//        prevButton.setBounds(pButton1X, buttonY, buttonWidth, buttonHeight);
-//    }
 
 }
